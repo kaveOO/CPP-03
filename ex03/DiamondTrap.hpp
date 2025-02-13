28e715a6f9a571455a518f5ef7ff68c120fcf3be
kaveOO <alban.billiette@gmail.com>
2025-02-13 11:13:52 +0100

    + | Have to finish last one

---
+++ ex03/DiamondTrap.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 11:08:52 by albillie          #+#    #+#             */
+/*   Updated: 2025/02/13 11:11:46 by albillie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DIAMOND_TRAP_HPP
+#define DIAMOND_TRAP_HPP
+
+#include "FragTrap.h"
+
+class DiamondTrap : public ScavTrap, public FragTrap
+{
+
+
+
+};
+
+
+#endif
